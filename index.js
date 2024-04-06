57c43c4a8c4cbbc17192b03787bdf5779297a383
Candice Thomason <candice.thomason@gmail.com>
2024-04-06 16:57:14 +1100

    3 out of 3

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+const cardNames = ["Guadalupe", "Ollie", "Aki"];
+
+//1.
+function writeCards(cardNames) {
+  const thankYouMessages = [];
+  for (let i = 0; i < cardNames.length; i++) {
+    thankYouMessages.push(
+      `Thank you, ${cardNames[i]}, for the wonderful surprise gift!`
+    );
+  }
+  return thankYouMessages;
+}
+
+const thankYouMessages = writeCards(cardNames);
+console.log(thankYouMessages); // Output the array of thank you messages
+
+//2.
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+//call the function
+countDown(10);
